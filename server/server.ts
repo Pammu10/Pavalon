db4955a51b840957e683d88b8b38738823ea1f20
Pammu10 <pramodh380@gmail.com>
2026-07-17 18:32:42 +0530

    feat(server): LiveKit voice token endpoint

--- server/server.ts
+++ server/server.ts
@@ -10,6 +10,7 @@ import webrtcRoutes from './routes/webrtc';
 import { createUserRouter } from './routes/user';
 import { createSocialRouter } from './routes/social';
 import { createAdminRouter } from './routes/admin';
+import { createVoiceRouter } from './routes/voice';
 import { registerSocketHandlers } from './socket/handlers';
 import { GameService } from './services/gameService';
 import { SocialService } from './services/socialService';
@@ -39,6 +40,7 @@ app.use('/api', createUserRouter(gameService));
 app.use('/api/social', createSocialRouter(socialService));
 app.use('/api/admin', createAdminRouter(gameService));
 app.use('/api/webrtc', webrtcRoutes);
+app.use('/api/voice', createVoiceRouter(gameService));
 
 app.use(errorHandler);
 
